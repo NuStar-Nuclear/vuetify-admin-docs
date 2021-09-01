a95c71cd3df73cee37fb6ca7186b62801240a4f3
Yue JIN <yjin@nustarnuclear.com>
2021-09-01 10:05:05 +0800

    🎨 Format config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,66 +1,64 @@
 module.exports = {
-  head: [
-    ['link', { rel: 'icon', href: 'nustar-vue.svg' }]
-  ],
-  lang: 'zh-CN',
-  base: '/nustar-admin-template-docs/',
-  title: 'NuStar Admin Frontend Template',
-  description: 'Documentation of NuStar Admin Frontend Template',
+  head: [["link", { rel: "icon", href: "nustar-vue.svg" }]],
+  lang: "zh-CN",
+  base: "/nustar-admin-template-docs/",
+  title: "NuStar Admin Frontend Template",
+  description: "Documentation of NuStar Admin Frontend Template",
   configureWebpack: {
     resolve: {
       alias: {
-        '@alias': 'path/to/some/dir',
+        "@alias": "path/to/some/dir",
       },
     },
   },
   themeConfig: {
-    logo: '/nustar-vue.svg',
+    logo: "/nustar-vue.svg",
     sidebar: [
       {
-        text: '指南', // 必要的
+        text: "指南", // 必要的
         collapsable: false, // 可选的, 默认值是 true,
         sidebarDepth: 1, // 可选的, 默认值是 1
-        link:'/guide/',
+        link: "/guide/",
         children: [
-          '/guide/',
-          '/guide/update',
-          '/guide/structure',
-          '/guide/routes',
-          '/guide/backendUrl',
-          '/guide/backendApi',
-          '/guide/vuex',
-          '/guide/mock',
-          '/guide/i18n',
-          '/guide/codeQuality',
-          '/guide/experiences',
-          '/guide/distribution',
+          "/guide/",
+          "/guide/update",
+          "/guide/structure",
+          "/guide/routes",
+          "/guide/backendUrl",
+          "/guide/backendApi",
+          "/guide/vuex",
+          "/guide/mock",
+          "/guide/i18n",
+          "/guide/codeQuality",
+          "/guide/experiences",
+          "/guide/distribution",
         ],
       },
       {
-        text: '组件', // 必要的
+        text: "组件", // 必要的
         collapsable: false, // 可选的, 默认值是 true,
         sidebarDepth: 1, // 可选的, 默认值是 1
         children: [
-          '/components/vldInputs',
-          '/components/dataListCard.md',
-          '/components/echarts',
+          "/components/vldInputs",
+          "/components/dataListCard.md",
+          "/components/echarts",
         ],
       },
       {
-        text: '测试', // 必要的
+        text: "测试", // 必要的
         collapsable: false, // 可选的, 默认值是 true,
         sidebarDepth: 1, // 可选的, 默认值是 1
-        children: ['/tests/unit', '/tests/e2e'],
+        children: ["/tests/unit", "/tests/e2e"],
       },
       {
-        text: '进阶', // 必要的
+        text: "进阶", // 必要的
         collapsable: false, // 可选的, 默认值是 true,
         sidebarDepth: 1, // 可选的, 默认值是 1
-        children: ['/advanced/material'],
+        children: ["/advanced/material"],
       },
       {
-        text: '实用方法',
-        link: '/utils/',
+        text: "实用方法",
+        link: "/utils/",
         collapsable: false,
         sidebarDepth: 2,
       },
@@ -68,17 +66,17 @@ module.exports = {
   },
   plugins: [
     [
-      '@vuepress/plugin-search',
+      "@vuepress/plugin-search",
       {
         locales: {
-          '/': {
-            placeholder: '搜索',
+          "/": {
+            placeholder: "搜索",
           },
-          '/en/': {
-            placeholder: 'Search',
+          "/en/": {
+            placeholder: "Search",
           },
         },
       },
     ],
   ],
-}
+};
